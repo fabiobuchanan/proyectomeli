4ba498f80b481ad1b1e69b159d3e337454eb064f
Fabio Buchanan <78232976+fabiobuchanan@users.noreply.github.com>
2024-04-26 10:40:20 -0300

    modals fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-
 // Sticky & Fixed Nav Bar.
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 let header = document.getElementById("menu-fixed");
 
@@ -17,177 +18,187 @@ function myFunction() {
   }
 }
 
-
 // LOGIN
 
 // Function to show the login modal
 function showLoginModal() {
-  document.getElementById('loginModal').style.display = 'block';
+  document.getElementById("loginModal").style.display = "block";
 }
 
 // Function to hide the login modal
 function hideLoginModal() {
-  document.getElementById('loginModal').style.display = 'none';
+  document.getElementById("loginModal").style.display = "none";
 }
 
 // Event listener for the login button in the header
-const login = document.getElementById('logInputBtn'); 
-  login.addEventListener('click', function() {
+const login = document.getElementById("logInputBtn");
+login.addEventListener("click", function () {
   showLoginModal();
 });
 
 // Event listener for the close button within the modal
-document.querySelector('.close').addEventListener('click', function() {
+document.querySelector(".close").addEventListener("click", function () {
   hideLoginModal();
 });
 
 // Event listener for clicks outside of the modal to close it
-window.addEventListener('click', function(event) {
-  let modal = document.getElementById('loginModal');
+window.addEventListener("click", function (event) {
+  let modal = document.getElementById("loginModal");
   if (event.target == modal) {
     hideLoginModal();
   }
 });
 
 // Event listener for the cancel button within the modal
-document.getElementById('cancelBtnLogin').addEventListener('click', function() {
-  hideLoginModal();
-});
+document
+  .getElementById("cancelBtnLogin")
+  .addEventListener("click", function () {
+    hideLoginModal();
+  });
 
 // Event listener for the login form submission
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault();  
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  const misCursos = document.getElementById('cursos2');
-  misCursos.classList.add('enabled');  
+    const misCursos = document.getElementById("cursos2");
+    misCursos.classList.add("enabled");
 
-  // Get the input values
-  let email = document.getElementById('username').value;
-  let password = document.getElementById('password').value;
+    // Get the input values
+    let email = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-  // Construct the request body
-  let requestBody = JSON.stringify({ email, password });
+    // Construct the request body
+    let requestBody = JSON.stringify({ email, password });
 
-  // Send the request to the server
-  fetch('http://localhost:8080/api/auth/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: requestBody
-  })
-  .then(response => {
-    if (response.ok) {
-     user = response.user; 
-    } else {
-      throw new Error('Network response was not ok.');
-    }
-  })
-  .then(data => {
-    // Handle the response from the server
-    if (data.success) {
-      // If login successful, show success message
-      document.getElementById('message').textContent = 'Login successful!';
-      // Hide the login modal after successful login
-      hideLoginModal();
-    } else {
-      // If login failed, show error message
-      document.getElementById('message').textContent = 'Invalid username or password. Please try again.';
-    }
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-    // Show error message if there's a problem with the fetch operation
-    document.getElementById('message').textContent = 'There was a problem with the login process. Please try again later.';
+    // Send the request to the server
+    fetch("http://localhost:8080/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: requestBody,
+    })
+      .then((response) => {
+        if (response.ok) {
+          user = response.user;
+        } else {
+          throw new Error("Network response was not ok.");
+        }
+      })
+      .then((data) => {
+        // Handle the response from the server
+        if (data.success) {
+          // If login successful, show success message
+          document.getElementById("message").textContent = "Login successful!";
+          // Hide the login modal after successful login
+          hideLoginModal();
+        } else {
+          // If login failed, show error message
+          document.getElementById("message").textContent =
+            "Invalid username or password. Please try again.";
+        }
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+        // Show error message if there's a problem with the fetch operation
+        document.getElementById("message").textContent =
+          "There was a problem with the login process. Please try again later.";
+      });
   });
-});
-
-
 
 // REGISTER
 
 // Function to show the login modal
 function showSignUpModal() {
-  document.getElementById('signUpModal').style.display = 'block';
+  document.getElementById("signUpModal").style.display = "block";
 }
 
 // Function to hide the login modal
 function hideSignUpModal() {
-  document.getElementById('signUpModal').style.display = 'none';
+  document.getElementById("signUpModal").style.display = "none";
 }
 
 // Event listener for the login button in the header
-const signUp = document.getElementById('signInputBtn');
-signUp.addEventListener('click', function() {
+const signUp = document.getElementById("signInputBtn");
+signUp.addEventListener("click", function () {
   showSignUpModal();
 });
 
 // Event listener for the close button within the modal
-document.querySelector('.close').addEventListener('click', function() {
+document.querySelector(".close").addEventListener("click", function () {
   hideSignUpModal();
 });
 
 // Event listener for clicks outside of the modal to close it
-window.addEventListener('click', function(event) {
-  let modal = document.getElementById('signUpModal');
+window.addEventListener("click", function (event) {
+  let modal = document.getElementById("signUpModal");
   if (event.target == modal) {
     hideSignUpModal();
   }
 });
 
 // Event listener for the cancel button within the modal
-document.getElementById('cancelBtnSignup').addEventListener('click', function() {
-  hideSignUpModal();
-});
+document
+  .getElementById("cancelBtnSignup")
+  .addEventListener("click", function () {
+    hideSignUpModal();
+  });
 
 // Event listener for the login form submission
-document.getElementById('signUpForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
+document
+  .getElementById("signUpForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+    
+    // Información para mandarle a César y que guarde al usuario, para luego poder hacer el LOGIN.
+    let firstName = document.getElementById("nombre").value;
+    let lastName = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let reingresePassword = document.getElementById(
+      "reingreseContraseña"
+    ).value;
 
-  // Información para mandarle a César y que guarde al usuario, para luego poder hacer el LOGIN.
-  let firstName = document.getElementById('nombre').value;
-  let lastName = document.getElementById('apellido').value;
-  let email = document.getElementById('email').value;
-  let password = document.getElementById('password').value;
-  let reingresePassword = document.getElementById('reingreseContraseña').value;
+    // Construct the request body
+    let requestBody = JSON.stringify({ firstName, lastName, email, password });
 
-  // Construct the request body
-  let requestBody = JSON.stringify({ firstName, lastName, email, password });
-
-  // Send the request to the server
-  fetch('http://localhost:8080/api/auth/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: requestBody
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Network response was not ok.');
-    }
-  })
-  .then(data => {
-    // Handle the response from the server
-    if (data.success) {
-      // If login successful, show success message
-      document.getElementById('message').textContent = 'Login successful!';
-      // Hide the login modal after successful login
-      hideSignUpModal();
-    } else {
-      // If login failed, show error message
-      document.getElementById('message').textContent = 'Invalid username or password. Please try again.';
-    }
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-    // Show error message if there's a problem with the fetch operation
-    document.getElementById('message').textContent = 'There was a problem with the login process. Please try again later.';
+    // Send the request to the server
+    fetch("http://localhost:8080/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: requestBody,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Network response was not ok.");
+        }
+      })
+      .then((data) => {
+        // Handle the response from the server
+        if (data.success) {
+          // If login successful, show success message
+          document.getElementById("message").textContent = "Login successful!";
+          // Hide the login modal after successful login
+          hideSignUpModal();
+        } else {
+          // If login failed, show error message
+          document.getElementById("message").textContent =
+            "Invalid username or password. Please try again.";
+        }
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+        // Show error message if there's a problem with the fetch operation
+        document.getElementById("message").textContent =
+          "There was a problem with the login process. Please try again later.";
+      });
   });
-});
-
 
 // MENU HAMBURGUESA - seleccionamos los dos elementos que serán clickables
 
@@ -209,11 +220,9 @@ toggleButton.addEventListener("click", () => {
   el menú debe esconderse.
 */
 
-navWrapper.addEventListener("click", e => {
+navWrapper.addEventListener("click", (e) => {
   if (e.target.id === "nav") {
     navWrapper.classList.remove("show");
     toggleButton.classList.remove("close");
   }
 });
-
-
