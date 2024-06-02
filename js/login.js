fc924b6b33e39b5342a000bd891d38a0e9161329
Fabio Buchanan <78232976+fabiobuchanan@users.noreply.github.com>
2024-06-02 17:38:56 -0300

    updates

--- js/login.js
+++ js/login.js
@@ -31,22 +31,57 @@ function showLoginModal() {
   document.getElementById("cancelBtnLogin").addEventListener("click", function () {
       hideLoginModal();
     });
+
+
   
-  // Event listener for the login form submission
+  // LOGIN 
   document.getElementById("loginForm").addEventListener("submit", function (event) {
       event.preventDefault();
   
-      const misCursos = document.getElementById("cursos2");
-      misCursos.classList.add("enabled");
-  
-      // Get the input values
       let email = document.getElementById("username").value;
       let password = document.getElementById("password").value;
-  
-      // Construct the request body
+      const uploadButton = document.getElementById("cargasAdmin");
+      const misCursos = document.getElementById("cursos2");
+
       let requestBody = JSON.stringify({ email, password });
   
-      // Send the request to the server
+      try {
+        const response = fetch('http://localhost:8080/api/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: requestBody,
+        });
+    
+      
+        if (response.ok) {
+            const result = response.json();
+            if (result.role === 'admin') {
+                alert('Admin login successful');
+                loginForm.reset();
+                document.getElementById('login-container').classList.add('hidden');
+                uploadButton.classList.remove('hidden');
+            } else if (result.role === 'cliente') {
+                alert('Customer login successful');
+                loginForm.reset();
+                document.getElementById('login-container').classList.add('hidden');
+                misCursos.classList.add("enabled");
+            } else {
+                alert('Unknown role');
+            }
+        } else {
+            alert('Invalid credentials');
+        }
+    } catch (error) {
+        console.error('Error during fetch operation:', error);
+        alert('An error occurred. Please try again.');
+    };
+
+uploadButton.addEventListener('click', () => {
+    uploadContainer.classList.toggle('hidden');
+});
+  });
+
+/*
       fetch("http://localhost:8080/api/auth/login", {
         method: "POST",
         headers: {
@@ -75,6 +110,5 @@ function showLoginModal() {
           document.getElementById("message").textContent =
             "There was a problem with the login process. Please try again later.";
         });
-    });
-  
-  
+    }); 
+  */
